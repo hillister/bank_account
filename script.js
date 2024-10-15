3b42db76ee24b93a175d2cc20405716403b150ee
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-10-15 12:19:02 +0100

    Add getBalance method

--- script.js
+++ script.js
@@ -7,19 +7,24 @@ class BankAccount {
     deposit(amount){
         let balance =  amount + this.balance
         this.balance = balance
-        console.log(`Your new balance is ${balance}`)
+        console.log(`You successfully deposited $${amount}`)
     }
 
     withdraw(amount){
         if(amount >= 5 && amount <= this.balance){
             let balance =  this.balance - amount
             this.balance = balance
-            console.log(`Your new balance is ${balance}`)
+            console.log(`You successfully withdrew $${amount}`)
         } else if(amount > this.balance){
             console.log(`You do not have enough money to withdraw this ammount`);
         } else {
             console.log(`The minimum withdrawl is $5`);
         }
+    }
+
+    getBalance(currentBalance){
+        currentBalance = this.balance;
+        console.log(`Your current balance is: $${currentBalance}`) 
     }
 }
 
@@ -28,5 +33,10 @@ const jerry = new BankAccount(69847)
 
 jerry.deposit(200);
 jerry.deposit(40);
-jerry.withdraw(300);
+jerry.withdraw(10);
 jerry.withdraw(2);
+
+jerry.getBalance();
+
+jerry.deposit(40);
+jerry.getBalance();

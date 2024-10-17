0205a989c4a1cd32401dd46fc9c93387726e6d0f
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-10-17 11:59:41 +0100

    form displays when deposit or withdrawl is clicked

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,6 +6,7 @@ class Dashboard {
             window.location.href = 'login.html';
         } else {
             this.loadUserData();
+            this.transactionButtons();
         }
     }
 
@@ -22,6 +23,64 @@ class Dashboard {
             accountNo.textContent = `Account number: ${userData.accountNumber}`;
 
             this.userAccount = userData
+        }
+    }
+
+    transactionButtons() {
+        const depositBtn = document.getElementById('deposit');
+        const withdrawlBtn = document.getElementById('withdrawl');
+        const transactionForm = document.getElementById('transactionForm');
+        const transactionMessage = document.getElementById('transactionMessage');
+        let transactionType = '';
+
+        depositBtn.addEventListener("click", () => {
+            transactionType = 'deposit';
+            this.showTransactionForm('Deposit Amount');
+        })
+
+
+        withdrawlBtn.addEventListener("click", () => {
+            transactionType = 'withdrawl';
+            this.showTransactionForm('Withdrawl Amount');
+        })
+
+        const transactionFormElement = document.getElementById('transactionFormElement');
+        transactionFormElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const amount = parseFloat(document.getElementById("transactionAmount").value);
+
+            if (transactionType === 'deposit') {
+                this.deposit(amount, transactionMessage);
+            } else if (transactionType === 'withdraw') {
+                this.withdraw(amount, transactionMessage);
+            }
+
+            this.updateBalance();
+        });
+    }
+
+    showTransactionForm(labelText) {
+        const transactionForm = document.getElementById("transactionForm");
+        const transactionAmountLabel = document.querySelector("label[for='amount']");
+        transactionAmountLabel.textContent = labelText;
+
+        document.getElementById("transactionFormElement").reset();
+        document.getElementById("transactionMessage").textContent = '';
+
+        transactionForm.classList.remove("hidden");
+    }
+
+    deposit(amount, transactionMessage) {
+        const account = new BankAccount(this.userAccount.username, this.userAccount.balance);
+        account.transactions = this.userAccount.transactions || [];
+        account.deposit(amount);
+
+        if (amount > 0) {
+            transactionMessage.textContent = `Successfully deposited $${amount.toFixed(2)}.`;
+            this.userAccount.balance = account.balance; 
+            this.userAccount.transactions = account.transactions; 
+        } else {
+            transactionMessage.textContent = `Deposit amount must be greater than zero.`;
         }
     }
 }

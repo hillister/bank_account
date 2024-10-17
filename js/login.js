634df38c45637ff947d3173180fd9a3f746c2da7
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-10-17 09:58:43 +0100

    log in form works

---
+++ js/login.js
@@ -0,0 +1,34 @@
+class Login {
+    constructor() {
+        const form = document.getElementById("loginForm");
+        form.addEventListener("submit", this.handleLogIn.bind(this))
+    }
+
+    handleLogIn(event){
+        event.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        if(this.validateLogin(username, password)){
+            alert(`Welcome back, ${username}!`);
+            window.location.href = 'dashboard.html';
+        } else {
+            alert(`Wrong username or password try again`);
+        }
+    }
+
+    validateLogin(username, password){
+        const storedUserData = JSON.parse(localStorage.getItem(username));
+
+        if(storedUserData && storedUserData.password === password){
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const login = new Login();  
+});
